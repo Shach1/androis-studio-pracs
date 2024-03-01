c19358405a878c75418af79efa12f9f37a9532c6
Evgeniy PC <79169380904@mail.ru>
2024-03-02 00:13:02 +0300

    finish 1 part of 4 pr

--- pr4/app/src/main/java/com/example/pr4/SecondActivity.java
+++ pr4/app/src/main/java/com/example/pr4/SecondActivity.java
@@ -1,14 +1,73 @@
 package com.example.pr4;
 
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
 
-public class SecondActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        Bundle arguments = getIntent().getExtras();
+        if (arguments != null)
+        {
+            User user = arguments.getParcelable(User.class.getSimpleName());
+
+            TextView nameView = findViewById(R.id.textViewName);
+            nameView.setText(getString( R.string.name) + user.getName());
+            TextView surnameView = findViewById(R.id.textViewSurname);
+            surnameView.setText(getString( R.string.surname) + user.getSurname());
+        }
+    }
+
+    static final String ACCESS_MSG = "ACCESS";
+
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult
+            (
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>()
+                {
+                    @Override
+                    public void onActivityResult(ActivityResult result)
+                    {
+                        TextView subjectView = findViewById(R.id.textViewSubject);
+                        if (result.getResultCode() == Activity.RESULT_OK)
+                        {
+                            Intent intent = result.getData();
+                            String subject = intent.getStringExtra(ACCESS_MSG);
+                            subjectView.setText(subject);
+                            Toast.makeText(getApplicationContext(), R.string.alert_ok, Toast.LENGTH_SHORT).show();
+                        }
+                        else Toast.makeText(getApplicationContext(), R.string.alert_canceled, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            );
+
+    public void onNextActivity(View view)
+    {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startForResult.launch(intent);
     }
 }
--- pr4/app/src/main/java/com/example/pr4/ThirdActivity.java
+++ pr4/app/src/main/java/com/example/pr4/ThirdActivity.java
@@ -2,7 +2,10 @@ package com.example.pr4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+    }
+
+    public void sendMessage(View view)
+    {
+        EditText dayEdit, timeEdit, commentEdit;
+        dayEdit = findViewById(R.id.editTextDay);
+        timeEdit = findViewById(R.id.editTextTime);
+        commentEdit = findViewById(R.id.editTextComment);
+
+        String day, time, comment;
+        day = dayEdit.getText().toString();
+        time = timeEdit.getText().toString();
+        comment = commentEdit.getText().toString();
+
+        Intent intent = new Intent();
+        if (!day.isEmpty() && !time.isEmpty() && !comment.isEmpty())
+        {
+            intent.putExtra(SecondActivity.ACCESS_MSG, day + " " + time + " " + comment);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        else
+        {
+            setResult(RESULT_CANCELED, intent);
+            finish();
+        }
     }
 }
