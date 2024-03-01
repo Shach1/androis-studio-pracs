e362099819e82bb24066705535a068bae0aa9eb6
Evgeniy PC <79169380904@mail.ru>
2024-03-01 22:17:53 +0300

    finish MainActivity

--- pr4/app/src/main/java/com/example/pr4/MainActivity.java
+++ pr4/app/src/main/java/com/example/pr4/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pr4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onNextActivity(View view)
+    {
+        EditText nameEdit = findViewById(R.id.editTextName);
+        EditText surnameEdit = findViewById(R.id.editTextSurname);
+        String name = nameEdit.getText().toString();
+        String surname = surnameEdit.getText().toString();
+
+        if (!name.isEmpty() && !surname.isEmpty())
+        {
+            User user = new User(name, surname);
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra(User.class.getSimpleName(), user);
+            startActivity(intent);
+        }
     }
 }
