2873e261c7d04bb75905ce235f6a6067d99fda19
Evgeniy PC <79169380904@mail.ru>
2024-03-27 01:58:01 +0300

    complete task 3 for practice 5

--- pr5/app/src/main/java/com/example/pr5/task3/MainActivityFor3.java
+++ pr5/app/src/main/java/com/example/pr5/task3/MainActivityFor3.java
@@ -3,6 +3,9 @@ package com.example.pr5.task3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.pr5.R;
 
@@ -12,5 +15,21 @@ public class MainActivityFor3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_for3);
+    }
+
+    public void onAddText(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.editTextText2);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        String textToAdd = editText.getText().toString();
+        String text = textView.getText().toString();
+
+        if (!textToAdd.isEmpty())
+        {
+            text += "\n    " + textToAdd;
+            editText.setText("");
+            textView.setText(text);
+        }
     }
 }
